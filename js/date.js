151d805b7411ecce92c8b5975199d041ecf6bbe4
Wil Helmina <106503631+xcelsi1@users.noreply.github.com>
2022-09-13 14:28:16 +0700

    Update date.js

--- js/date.js
+++ js/date.js
@@ -1,51 +1,23 @@
+function updateTimer() {
+  future  = Date.parse("November 1, 2021 00:00:00");
+  now     = new Date();
+  diff    = future - now;
 
-var dv = document.getElementById("content");
-dv.style.opacity = 0;
-var val = 0;
+  days  = Math.floor( diff / (1000*60*60*24) );
+  hours = Math.floor( diff / (1000*60*60) );
+  mins  = Math.floor( diff / (1000*60) );
+  secs  = Math.floor( diff / 1000 );
 
-function timer(){
-	var start = new Date(2018, 0, 27, 20, 53);
-	var t = new Date() - start;
-	var d = Math.floor(t / 1000 / 60 / 60 / 24);
-	var h = Math.floor(t / 1000 / 60 / 60 % 24);
-	if(h < 10){
-		h = "0" + h;
-	}
-	var m = Math.floor(t / 1000 / 60 % 60);
-	if(m < 10){
-		m = "0" + m;
-	}
-	var s = Math.floor(t / 1000 % 60);
-	if(s < 10){
-		s = "0" + s;
-	}
-	document.getElementById("d").innerHTML = d;
-	document.getElementById("h").innerHTML = h;
-	document.getElementById("m").innerHTML = m;
-	document.getElementById("s").innerHTML = s;
+  d = days;
+  h = hours - days  * 24;
+  m = mins  - hours * 60;
+  s = secs  - mins  * 60;
+
+  document.getElementById("timer")
+    .innerHTML =
+      '<div>' + d + '<span>days</span></div>' +
+      '<div>' + h + '<span>hours</span></div>' +
+      '<div>' + m + '<span>minutes</span></div>' +
+      '<div>' + s + '<span>seconds</span></div>' ;
 }
-
-function fadein(){
-	if(val < 1){
-		val += 0.025;
-		dv.style.opacity = val;
-	}
-	else{
-		clearInterval(fadeinInterval);
-		if(ok == 2){
-			ok += 1;
-		}
-	}
-}
-
-var fadeInterval;
-var fadeinInterval;
-
-timer();
-setInterval(timer, 1000);
-fadeInterval = setInterval(function(){
-	if(ok == 2){
-		clearInterval(fadeInterval);
-		fadeinInterval = setInterval(fadein, 50);
-	}
-}, 50)
+setInterval('updateTimer()', 1000 );
