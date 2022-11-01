1bde1534df8c5e167813fba0012d6e95bff61b28
Wil Helmina <106503631+xcelsi1@users.noreply.github.com>
2022-11-01 14:45:55 +0700

    Update typeWriter.js

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,8 +1,7 @@
 
 let i = 0;
 let text1 = "To my beloved princess, Randa. happy anniversary ";
-let text2 = "I feel so very lucky to know you, and very lucky to have you absolutely. falling in love with you really makes me happy."
-"i never thought i could find a girlfriend like you, but look now i found the best one in the world, yeah its u. i always choose you no matter what happens, i love everything about u, kamu sempurna..sorry for always make u mad bubu"
+let text2 = "I feel so very lucky to know you, and very lucky to have you absolutely. falling in love with you really makes me happy. i never thought i could find a girlfriend like you, but look now i found the best one in the world, yeah its u. i always choose you no matter what happens, i love everything about u, kamu sempurna i love u, i love u so much, i love u more then u even know, kamu lucu banget. thank u for being part of my journey, being with u and loving u has made me happier than I had ever hoped to be. I cannot wait to spend a lifetime making memories with u"
 let speed = 120;
 
 function typeWriter(text, para){
